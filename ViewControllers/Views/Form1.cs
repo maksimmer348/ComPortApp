fa9292596a6d0919e2897daafaee65d007742e42
maksimmer348 <maksimmer348@gmail.com>
2020-11-14 17:01:57 +0300

    added Form1

---
+++ ViewControllers/Views/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using ZedGraph;
+using System.Timers;
+
+namespace ComPortApp
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //класс для исеользования графика
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
